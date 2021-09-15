106d03b5d75c411ecfde8b72e5527920a9eaf941
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-15 17:29:07 -0300

    Backend Version Created

---
+++ backend/src/test/java/com/rrm14/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrm14.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
