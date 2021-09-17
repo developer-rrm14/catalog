4978f945fbe835c5b8a0771379eda62945eb0653
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-17 03:00:10 -0300

    Add Exceptions

---
+++ backend/src/main/java/com/rrm14/catalog/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.rrm14.catalog.constants;
+
+public class Constants {
+
+	public static final String  RESOURCE_NOT_FOUND = "Resource Not Found";
+	public static final String  ENTITY_NOT_FOUND = "Entity Not Found";
+}
--- backend/src/main/java/com/rrm14/catalog/services/CategoryService.java
+++ backend/src/main/java/com/rrm14/catalog/services/CategoryService.java
@@ -1,15 +1,18 @@
 package com.rrm14.catalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.rrm14.catalog.constants.Constants;
 import com.rrm14.catalog.dto.CategoryDTO;
 import com.rrm14.catalog.entities.Category;
 import com.rrm14.catalog.repositories.CategoryRepository;
+import com.rrm14.catalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,5 +25,12 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		return list.stream().map(item-> new CategoryDTO(item)).collect(Collectors.toList());
 				
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(()-> new EntityNotFoundException(Constants.ENTITY_NOT_FOUND));
+		return new CategoryDTO(entity);
 	}
 }
