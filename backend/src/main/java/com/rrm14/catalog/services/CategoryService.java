0314a5807583b336f5211a9e97f31fb695fc1be1
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-17 02:11:05 -0300

    Add DTO Layer

--- backend/src/main/java/com/rrm14/catalog/services/CategoryService.java
+++ backend/src/main/java/com/rrm14/catalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.rrm14.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.rrm14.catalog.dto.CategoryDTO;
 import com.rrm14.catalog.entities.Category;
 import com.rrm14.catalog.repositories.CategoryRepository;
 
@@ -16,7 +18,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(item-> new CategoryDTO(item)).collect(Collectors.toList());
+				
 	}
 }
