24eeb76583e4e7c29dff51675c2c1ddf0d65880c
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-23 18:59:18 -0300

    Refactor Pageable

--- backend/src/main/java/com/rrm14/catalog/dto/ProductDTO.java
+++ backend/src/main/java/com/rrm14/catalog/dto/ProductDTO.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.Column;
-
 import com.rrm14.catalog.entities.Category;
 import com.rrm14.catalog.entities.Product;
 
