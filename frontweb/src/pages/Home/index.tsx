8e04fcf9f11340306940f61d0d4899df26d9973c
moduretick <robertoreboucasdemelo@gmail.com>
2021-11-22 15:10:55 -0300

    Check User Authenticated

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
+import { getTokenData, isAuthenticated } from 'utils/requests';
 import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
+      <h1>{isAuthenticated() ? 'autenticado' : 'Não Autenticado'}</h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
